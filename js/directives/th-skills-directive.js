3be55a644dcd8783a0da9b67d61656e15cc650c1
Tim Holmes <tholmes@starchase.com>
2015-12-31 12:17:43 -0500

    Added some new icons, data to skills section, skills functionality, and work layout. Changed background image in education section.

--- js/directives/th-skills-directive.js
+++ js/directives/th-skills-directive.js
@@ -22,33 +22,76 @@
 		  	"id": "1",
 		  	"name": "JavaScript",
 		    "url": "./imgs/icons/javascript.svg",
-		    "time": "1.5 years",
-		    "rank": 7,
-		    "descrip": "Live Data"
+		    "time": "8 months",
+		    "descrip": "JavaScript is what I've had the most consistent exposure to professionally, as well as where I like to spend my time. at StarChase, most of my projects have used JavaScript. For the email processor I used JavaScript to manipulate data before sending it to the back end. I've exclusively used JavaScript to interact with the Google Maps API.",
+		    "projects": [
+		    	{
+		    		"name": "StarChase Email Processor"
+		    	},
+		    	{
+		    		"name": "StarChase R&D"
+		    	},
+		    	{
+		    		"name": "STEM Website"
+		    	},
+		    	{
+		    		"name": "Personal Website"
+		    	}
+		    ]
 		  },
 		  {
 		  	"id": "2",
 		  	"name": "PHP",
 		    "url": "./imgs/icons/php.svg",
-		    "time": "1.5 years",
-		    "rank": 7,
-		    "descrip": "Quering DB"
+		    "time": "1 year",
+		    "descrip": "All of the web applications I've worked on at StarChase have had to extensively interact with a database. PHP came into play here, and I almost exclusively used it to query the database, and serve the data to the front end as an XML or JSON document. For the StarChase task managaer, I used PHP to send out various email notifications to users.",
+		    "projects": [
+		    	{
+		    		"name": "StarChase Email Processor"
+		    	},
+		    	{
+		    		"name": "StarChase Task Manager"
+		    	},
+		    	{
+		    		"name": "StarChase R&D"
+		    	},
+		    	{
+		    		"name": "STEM Website"
+		    	},
+		    	{
+		    		"name": "Personal Website"
+		    	}
+		    ]
 		  },
 		  {
 		  	"id": "3",
 		  	"name": "Python",
 		    "url": "./imgs/icons/python.svg",
-		    "time": "1.5 years",
-		    "rank": 7,
-		    "descrip": "Parsing / TCP"
+		    "time": "6 months",
+		    "descrip": "While at StarChase, I've had to develop a couple back end scripts to process data, and I've used python for the most of these implementations. I developed one script that pulls mail from an exchange account, parses that data based on some pre-defined formats, and then inserts the data into a database. I've worked on another script that listens to a range of sockets and establishes a connection with a device that makes contact using TCP.",
+		    "projects": [
+		    	{
+		    		"name": "StarChase Email Processor"
+		    	},
+		    	{
+		    		"name": "StarChase Socket Listener"
+		    	}
+		    ]
 		  },
 		  {
 		  	"id": "4",
 		  	"name": "Java",
 		    "url": "./imgs/icons/java.svg",
-		    "time": "1.5 years",
-		    "rank": 7,
-		    "descrip": "Senior Project"
+		    "time": "1 year",
+		    "descrip": "I used Java in a few different courses at ODU, but really had an oppurtunity to get my hand dirty with it during my senior project. I worked on a team of students that developed a program that could display and manipulate genome files. We extensively utilized the BioJava API in order to develop this program. My contribution was implementing the file exporting, and change saving functionality.",
+		    "projects": [
+		    	{
+		    		"name": "Unverified Join Viewer"
+		    	},
+		    	{
+		    		"name": "Virtual File System Simulator"
+		    	}
+		    ]
 		  }
 		]
 
@@ -58,40 +101,96 @@
 		  	"name": "AngularJS",
 		    "url": "./imgs/icons/angular.svg",
 		    "time": "2 months",
-		    "rank": 3,
-		    "descrip": "AngularJS is my favorite framework of recent memory. I've used it for two of my three major projects at StarChase, and it has allowed me to create web pages that truly behave like applications. I feel comfortable using Angular, but I am still pretty new to it. I keep the API open while I'm developing. This website was built with Angluar!"
+		    "descrip": "AngularJS is my favorite framework of recent memory. I've used it for two of my three major projects at StarChase, and it has allowed me to create web pages that truly behave like applications. I feel comfortable using Angular, but I am still pretty new to it. I keep the API documentation open while I'm developing. This website was built with Angluar!",
+		    "projects": [
+		    	{
+		    		"name": "StarChase Email Processor"
+		    	},
+		    	{
+		    		"name": "StarChase Task Manager"
+		    	},
+		    	{
+		    		"name": "StarChase R&D"
+		    	},
+		    	{
+		    		"name": "Personal Website"
+		    	}
+		    ]
 		  },
 		  {
 		  	"id": 2,
 		  	"name": "HTML5/CSS3",
-		    "url": "./imgs/icons/html5_2.svg",
+		    "url": "./imgs/icons/html_this.png",
 		    "time": "5 years",
-		    "rank": 6,
-		    "descrip": "HTML/CSS is what got me thinking about programming. Truly, I've been tinkering with these frameworks for the majority of my life. I spent a lot of time as a kid buildiing themes for social networks (MySpace and Tumblr.) Professionally, every computer related job I've encountered as required editing webpages. I played around with new features introduced in HTML5/CSS3, but haven't interacted with them enough to know how to use them without reference."
+		    "descrip": "HTML/CSS is what got me thinking about programming. Truly, I've been tinkering with these frameworks for the majority of my life. I spent a lot of time as a kid buildiing themes for social networks (MySpace and Tumblr.) Professionally, every computer related job I've encountered as required editing webpages. I played around with new features introduced in HTML5/CSS3, but haven't interacted with them enough to know how to use them without reference.",
+		    "projects": [
+		    	{
+		    		"name": "StarChase Email Processor"
+		    	},
+		    	{
+		    		"name": "StarChase Task Manager"
+		    	},
+		    	{
+		    		"name": "StarChase R&D"
+		    	},
+		    	{
+		    		"name": "Personal Website"
+		    	}
+		    ]
 		  },
 		  {
 		  	"id": 2,
 		  	"name": "HTML5/CSS3",
-		    "url": "./imgs/icons/css.svg",
-		    "time": "1.5 years",
-		    "rank": 3,
-		    "descrip": ""
+		    "url": "./imgs/icons/css_this.svg",
+		    "time": "5 years",
+		    "descrip": "HTML/CSS is what got me thinking about programming. Truly, I've been tinkering with these frameworks for the majority of my life. I spent a lot of time as a kid buildiing themes for social networks (MySpace and Tumblr.) Professionally, every computer related job I've encountered as required editing webpages. I played around with new features introduced in HTML5/CSS3, but haven't interacted with them enough to know how to use them without reference.",
+		    "projects": [
+		    	{
+		    		"name": "StarChase Email Processor"
+		    	},
+		    	{
+		    		"name": "StarChase Task Manager"
+		    	},
+		    	{
+		    		"name": "StarChase R&D"
+		    	},
+		    	{
+		    		"name": "Personal Website"
+		    	}
+		    ]
 		  },
 		  {
 		  	"id": 3,
 		  	"name": "Bootstrap",
 		    "url": "./imgs/icons/bootstrap.svg",
 		    "time": "8 months",
-		    "rank": 4,
-		    "descrip": "In our modern world, responsive design is critical. Bootstrap makes designing web applications that adjust to any screen size easy. My mentor/supervisor at StarChase was keen that everything we built needed to be built with Bootstrap. "
+		    "descrip": "In our modern world, responsive design is critical. Bootstrap makes designing web applications that adjust to any screen size easy. My mentor/supervisor at StarChase was keen that everything we built needed to be built with Bootstrap. ",
+		    "projects": [
+		    	{
+		    		"name": "StarChase Email Processor"
+		    	},
+		    	{
+		    		"name": "StarChase Task Manager"
+		    	},
+		    	{
+		    		"name": "StarChase R&D"
+		    	},
+		    	{
+		    		"name": "Personal Website"
+		    	}
+		    ]
 		  },
 		  {
 		  	"id": 4,
 		  	"name": "Google Maps API V3",
 		    "url": "./imgs/icons/map1.png",
 		    "time": "8 months",
-		    "rank": 6,
-		    "descrip": "Google offers an service with their mapping API. I've been in an out of this API, and the functionality they offer is extremely impressive. NDAs keep me from being too specific about what I've done with this API. Here's a small map of some resturants I recommend:"
+		    "descrip": "Google offers an service with their mapping API. I've been in an out of this API, and the functionality they offer is extremely impressive. NDAs keep me from being too specific about what I've done with this API. Here's a small map of some resturants I recommend:",
+		    "projects": [
+		    	{
+		    		"name": "StarChase R&D"
+		    	}
+		    ]
 		  }
 		]
 
@@ -101,14 +200,12 @@
 		  	"name": "GitLab CE / Git",
 		    "url": "./imgs/icons/gitlab.png",
 		    "time": "1.5 years",
-		    "rank": 7,
 		    "descrip": "While I was at ODU, one of my professors told me that good programming \"doesn't happen in a vacuum.\" During my academic and professional career, I've taken this philosophy to heart. Collaboration is crucial when developing as a team. Any development I do, I keep in a git repository. While working at StarChase, I initialized and maintained a Gitlab CE server so our team could properly collaborate."
 		  },
 		  {
 		  	"id": 2,
 		  	"name": "Sublime",
 		    "time": "1 year",
-		    "rank": 7,
 		    "url": "./imgs/icons/Sublime_Text_Logo.png",
 		    "descrip": "Sublime Text is quick, responsive, and supported by a large development community that enhances the functionality beyond imagination. The current plugins I use with Sublime are: Package Control, Sublime SFTP, and DocBlockr."
 		  },
@@ -116,9 +213,19 @@
 		  	"id": 3,
 		  	"name": "MySql",
 		    "time": "1 year",
-		    "rank": 4,
 		    "url": "./imgs/icons/sql.svg",
-		    "descrip": "While at ODU, one of my electives was in an entry level relational database course. There I learned how to query, how joins work, what schemas look like, primary/foreign keys, etc. At StarChase, I had the oppurtunity to get some hands on experience with SQL. In this position I've had experience with integrating databases into a web application (both entering data and retrieving data), defining database and table characteristics, and accessing remote databases. I've interacted with them via the terminal, and PHPmyAdmin."
+		    "descrip": "While at ODU, one of my electives was in an entry level relational database course. There I learned how to query, how joins work, what schemas look like, primary/foreign keys, etc. At StarChase, I had the oppurtunity to get some hands on experience with SQL. In this position I've had experience with integrating databases into a web application (both entering data and retrieving data), defining database and table characteristics, and accessing remote databases. I've interacted with them via the terminal, and PHPmyAdmin.",
+		    "projects": [
+		    	{
+		    		"name": "StarChase Email Processor"
+		    	},
+		    	{
+		    		"name": "StarChase Task Manager"
+		    	},
+		    	{
+		    		"name": "StarChase R&D"
+		    	}
+		    ]
 		  }
 		]
 
