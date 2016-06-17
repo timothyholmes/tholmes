7df95ae7287e4b83dcc860e8c51e1487a61e4651
Tim Holmes <timothy.holmes@homes.com>
2016-06-17 16:08:45 -0400

    Reworked build and file structure

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,63 @@
-var gulp = require('gulp');
-var sass = require('gulp-sass');
+var Promise = require('es6-promise').Promise,
+    gulp = require('gulp'),
+    sass = require('gulp-sass'),
+    useref = require('gulp-useref'),
+    uglify = require('gulp-uglify'),
+    gulpIf = require('gulp-if'),
+    cssnano = require('gulp-cssnano'),
+    del = require('del'),
+    runSequence = require('run-sequence'),
+    connect = require('gulp-connect');
+
+gulp.task('move-assets', function(){
+    return gulp.src('./src/assets/**/*')
+    .pipe(gulp.dest('./dist/assets/'));
+});
 
 gulp.task('sass', function(){
-  return gulp.src('scss/**/*.scss')
-    .pipe(sass()) // Converts Sass to CSS with gulp-sass
-    .pipe(gulp.dest('css'))
+    return gulp.src('src/scss/**/*.scss')
+    .pipe(sass())
+    .pipe(gulp.dest('src/css'));
 });
 
 gulp.task('watch', function(){
-  gulp.watch('scss/**/*.scss', ['sass']); 
-})
+    gulp.watch('src/scss/**/*.scss', ['sass']);
+});
+
+gulp.task('useref', function(){
+    return gulp.src('src/index.html')
+    .pipe(useref())
+    .pipe(gulpIf('*.js', uglify({mangle: false})))
+    .pipe(gulpIf('*.css', cssnano()))
+    .pipe(gulp.dest('dist'));
+});
+
+gulp.task('clean', function() {
+    return del.sync('dist');
+});
+
+gulp.task('build', function (callback) {
+    runSequence('clean',
+        'sass',
+        'useref',
+        'move-assets',
+        callback
+    );
+});
+
+gulp.task('webserver', function() {
+    connect.server({
+        root: './dist/',
+        port: 8080
+    });
+    connect.server({
+        root: './src/',
+        port: 9090
+    });
+});
+
+gulp.task('default', function (callback) {
+    runSequence(['build', 'webserver', 'watch'],
+        callback
+    );
+});
